54afd01f0d5a8c60214878f0dcbc06372610a464
Jacob Williams <47766188+BotSpace@users.noreply.github.com>
2024-04-17 01:21:43 -0400

    added XRP tab + field listing, changed products to MiniFRC

---
+++ XRP/xrp-field.js
@@ -0,0 +1,45 @@
+let products = [
+    {
+        name: "Mountain Mayhem DIY Field Kit",
+        link: "mountain-mayhem-field-diy",
+        image: "xrp-field-diy.jpg",
+        price: "$145.00",
+        buy: `<strong style="color: red">Out of stock.</strong><br/>`,
+        blurb: `The hardest to source parts for the Mountain Mayhem game field.`,
+        description: `Mountain Mayhem is an exciting and accessible robot game designed for the <a href="https://www.sparkfun.com/products/22230">XRP platform</a>. Created in collaboration with FIRST, Mountain Mayhem is the perfect arena to hone one's roboting skills. Good for educators, students, FIRST teams, hobbyists, and more. In this game, take on the challenge of clearing the slide area, scoring snow in the reservoirs, scoring rocks in the quarry, and parking on the pass at the end of the game for big bonus points!
+        <br/><br/>
+        <strong>This bundle contains:</strong>
+        <div style="margin-left:10px">
+            <ul class="browser-default">
+            <li>1 - pass top (hardboard)</li>
+            <li>2 - pass slope (hardboard)</li>
+            <li>2 - reservoir basin (polycarbonate)</li>
+            <li>2 - reservoir slope (hardboard)</li>
+            <li>8 - slope wall (polycarbonate)</li>
+            <li>50 - #6-32 0.5in countersunk screw</li>
+            <li>30 - #6-32 0.5in button head screw</li>
+            <li>10ft - white gaffers tape</li>
+            <li>10ft - red gaffers tape</li>
+            <li>10ft - blue gaffers tape</li>
+        </div>
+
+        <br/><br/>
+        <strong>Resources:</strong>
+        <div style="margin-left:10px">
+            <ul class="browser-default">
+            <li><a href="https://cad.onshape.com/documents/96a67694c959c5256ba3888e/w/d53a317a26ca35360c404af6/e/cd4bd5a4145918ba604ba612?bomType=flattened&renderMode=0&rightPanel=BOMPanel&uiState=6618cac3be94de6a65ae8d71">Field CAD</a></li>
+            <li><a href="https://docs.google.com/document/d/1RmumBkXWaTyRvJN086SgkmbpiR8PneCvn1Q0SXBj8KA/edit?usp=sharing">Field Build Guide</a></li>
+        </div>
+
+        <br/><br/>
+        <strong>Must be sourced elsewhere:</strong>
+        <div style="margin-left:10px">
+            <ul class="browser-default">
+            <li>3d-printed parts. Printing the field and game pieces takes about two kilograms of filament. Download field parts from Printables!</li>
+            <li>6 sections of 0.5in schedule 40 PVC pipe, each 4ft long, can be bought and cut at any hardware store.</li>
+            
+        </div>
+
+        <em>Note: Lead times are dependant on stock. Early orders will be shipped immediately. After that the shipping delay may increase, up to but no longer than 2 weeks.</em>`
+    }
+]
